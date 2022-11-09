8bf8d38310b119b286f240b506c67d5cde89e55a
63070061 <63070061@kmitl.ac.th>
2022-11-09 15:31:50 +0700

    fake edit

--- App.js
+++ App.js
@@ -3,17 +3,8 @@ import { StyleSheet, View } from 'react-native';
 import Register from './frontend/reg';
 import Main_Page from './frontend/MainPage';
 import Login from './frontend/LoginPage';
-<<<<<<< HEAD
-import MyNavigator from './navigation/Navigator';
-
-export default function App() {
-  return (
-    // <View style={styles.container}>
-      
-    // </View>
-    <MyNavigator/>
-=======
 import CreateNewParty from './frontend/CreateParty';
+import MyNavigator from './navigation/Navigator';
 import './assets/base.css'
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
@@ -21,13 +12,8 @@ import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 export default function App() {
   return (
     <ApplicationProvider {...eva} theme={eva.dark}>
-      <View style={styles.container}>
-        {/* <Register></Register> */}
-        <Register></Register>
-        {/* <Text>testsetset</Text> */}
-      </View>
+      <MyNavigator />
     </ApplicationProvider>
->>>>>>> c91ed27f9a6b206f86c906dbb74eabb5434f5b81
   );
 }
 
@@ -37,6 +23,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#f5f5f5',
     alignItems: 'center',
     justifyContent: 'center',
-    
+
   },
 });
