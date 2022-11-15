bb623b0cf494c4d8759a11edfee4bb14dd634129
praew <63070012@kmitl.ac.th>
2022-11-15 18:05:15 +0700

    Join party

--- frontend/FindParty.js
+++ frontend/FindParty.js
@@ -4,20 +4,24 @@ import { Layout, Tab, TabView, Text, Input, Button, Card } from '@ui-kitten/comp
 import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
 import { OpenSans_500Medium, } from '@expo-google-fonts/open-sans';
 import { Kanit_400Regular } from '@expo-google-fonts/kanit';
+import { collection, getDoc, doc, getDocs, onSnapshot, setDoc } from "firebase/firestore";
+import { db } from '../firebase/firebase-config';
 
 
 import Searchbar from '../assets/component/searchbar';
+import { async } from '@firebase/util';
 
 const FindParty = ({ navigation }) => {
 
   const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const [code, setCode] = useState(0);
   const [data, setData] = useState([
     { id: 1, name: "เราพวกผองชาวสจล.ไปหาข้าวกิน", description: "เป็นปาร์ตี้ปลุกความขยันในตัวคุณหากคุณเคยประสบปัญหาการลืมตั้งนาฬิกาปลุก ทำให้ไปเข้าเรียนสายบ่อยครั้ง" },
     { id: 2, name: "ไปเรียนคณะกันชาวไอที", description: "เป็นปาร์ตี้ปลุกความขยันในตัวคุณหากคุณเคยประสบปัญหาการลืมตั้งนาฬิกาปลุก ทำให้ไปเข้าเรียนสายบ่อยครั้ง" },
     { id: 3, name: "ไปเตะบอลกัน", description: "เป็นปาร์ตี้ปลุกความขยันในตัวคุณหากคุณเคยประสบปัญหาการลืมตั้งนาฬิกาปลุก ทำให้ไปเข้าเรียนสายบ่อยครั้ง" },
     { id: 4, name: "ไปตลาดหอในกัน", description: "เป็นปาร์ตี้ปลุกความขยันในตัวคุณหากคุณเคยประสบปัญหาการลืมตั้งนาฬิกาปลุก ทำให้ไปเข้าเรียนสายบ่อยครั้ง" },
     { id: 5, name: "เล่นเกมกันเพื่อนๆ", description: "เป็นปาร์ตี้ปลุกความขยันในตัวคุณหากคุณเคยประสบปัญหาการลืมตั้งนาฬิกาปลุก ทำให้ไปเข้าเรียนสายบ่อยครั้ง" },
-])
+  ])
   let [fontsLoaded] = useFonts({
     Inter_900Black, OpenSans_500Medium, Kanit_400Regular
 
@@ -27,135 +31,178 @@ const FindParty = ({ navigation }) => {
     return null;
   }
 
-  return (
-    <TabView style={[styles.tabView]}
-      selectedIndex={selectedIndex}
-      onSelect={index => setSelectedIndex(index)}>
-      <Tab title='PUBLIC PARTY' style={{ backgroundColor: 'white' }}>
-        <Layout style={styles.tabContainer}>
-          <Searchbar></Searchbar>
-          <View style={styles.containerFilter}>
-            <Text category='h1' style={[styles.fontTh, { color: '#FDC319', paddingRight: '150px' }]}>หาปาร์ตี้</Text>
-            <Image source={require('../assets/filter_icon.png')} style={{ width: 30, height: 30 }} />
-          </View>
-          <View style={styles.containerCardparty}>
-            {data.map((item, index) =>
-              <View style={[{ paddingBottom: '10px' }]}>
-                <View style={[styles.row, styles.card]}>
-                  <View style={[styles.column3, { padding: 5 }]}>
-                    <Image source={require('../assets/foodparty_icon.png')} style={{ width: "100%", aspectRatio: "1/1", objectFit: "cover" }} />
-                  </View>
-                  <View style={[styles.column9]}>
-                    <Text style={[styles.fontTh, { color: '#4542C1', fontSize: '13px'}]}>{item.name}</Text>
+  // let partyList = []
+  //   let entered
+  //   const partySnapshot = getDocs(collection(db, "parties"));
+  //   // partySnapshot.forEach((doc) => {
+  //   //   partyList.push(doc.data())
+  //   // });
+  //   // console.log(partyList)
+  //   console.log(partySnapshot)
+
+  const joinParty = async () => {
+    let user
+    const username = localStorage.getItem("Username")
+    const ref = doc(db, "users", username);
+    const snap = await getDoc(ref);
+    if (snap.exists()) {
+      user = snap.data()}
+
+    let partyList = []
+    let entered
+    const partySnapshot = await getDocs(collection(db, "parties"));
+    partySnapshot.forEach((doc) => {
+      partyList.push(doc.data())
+    });
+    console.log(partyList)
+
+    //CHECK ENTER CODE WITH PARTY LIST
+    partyList.forEach((party) => {
+      if (code == party.enterCode) {
+        entered = party
+        console.log(entered)
+        user.party.push(party.partyName)
+        console.log(user.party)
+        return;
+      }
+    })
+
+    //ADD PARTY TO USER
+    const docRef = await setDoc(doc(db, "users",username), {
+      ...user
+  });
+  }
+
+
+    return (
+      <TabView style={[styles.tabView]}
+        selectedIndex={selectedIndex}
+        onSelect={index => setSelectedIndex(index)}>
+        <Tab title='PUBLIC PARTY' style={{ backgroundColor: 'white' }}>
+          <Layout style={styles.tabContainer}>
+            <Searchbar></Searchbar>
+            <View style={styles.containerFilter}>
+              <Text category='h1' style={[styles.fontTh, { color: '#FDC319', paddingRight: '150px' }]}>หาปาร์ตี้</Text>
+              <Image source={require('../assets/filter_icon.png')} style={{ width: 30, height: 30 }} />
+            </View>
+            <View style={styles.containerCardparty}>
+              {data.map((item, index) =>
+                <View style={[{ paddingBottom: '10px' }]}>
+                  <View style={[styles.row, styles.card]}>
+                    <View style={[styles.column3, { padding: 5 }]}>
+                      <Image source={require('../assets/foodparty_icon.png')} style={{ width: "100%", aspectRatio: "1/1", objectFit: "cover" }} />
+                    </View>
+                    <View style={[styles.column9]}>
+                      <Text style={[styles.fontTh, { color: '#4542C1', fontSize: '13px' }]}>{item.name}</Text>
+                    </View>
                   </View>
                 </View>
-              </View>
-            )}
-          </View>
-        </Layout>
-      </Tab>
-      <Tab title='PRIVATE PARTY' style={{ backgroundColor: 'white' }}>
-        <Layout style={styles.tabContainer}>
-          <Text category='h1' style={[styles.fontTh, { color: '#FDC319', }]}>ปาร์ตี้ส่วนตัว</Text>
-          <Text category='h6' style={[styles.fontTh, { color: '#4542C1', }]}>โค้ดสำหรับเข้าร่วมปาร์ตี้ส่วนตัว :</Text>
-          <Input style={[styles.fontEng, styles.fontEngInput, { backgroundColor: '#D9D9D9' }]} onChangeText={text => setUsername(text)} />
-          <Button style={[styles.fontEng, styles.buttonStyle, { margin: 10 }]}>join</Button>
-        </Layout>
-      </Tab>
-    </TabView>
-  );
-};
+              )}
+            </View>
+          </Layout>
+        </Tab>
+        <Tab title='PRIVATE PARTY' style={{ backgroundColor: 'white' }}>
+          <Layout style={styles.tabContainer}>
+            <Text category='h1' style={[styles.fontTh, { color: '#FDC319', }]}>ปาร์ตี้ส่วนตัว</Text>
+            <Text category='h6' style={[styles.fontTh, { color: '#4542C1', }]}>โค้ดสำหรับเข้าร่วมปาร์ตี้ส่วนตัว :</Text>
+            <Input style={[styles.fontEng, styles.fontEngInput, { backgroundColor: '#D9D9D9' }]} onChangeText={text => setCode(text)} />
+            <Button style={[styles.fontEng, styles.buttonStyle, { margin: 10 }]} onPress={joinParty}>join</Button>
+          </Layout>
+        </Tab>
+      </TabView>
+    );
+  };
 
-const styles = StyleSheet.create({
-  tabView: {
-    backgroundColor: 'white',
-    flex: 1,
+  const styles = StyleSheet.create({
+    tabView: {
+      backgroundColor: 'white',
+      flex: 1,
 
-  },
-  tabContainer: {
-    height: '100%',
-    width: '100%',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-    paddingTop: '20px'
-  },
-  tabBar: {
-    backgroundColor: 'white',
-  },
-  fontEng: {
-    fontFamily: 'Kanit_400Regular',
-    fontSize: 14,
-  },
-  fontEngInputHeader: {
-    fontFamily: 'Kanit_400Regular',
-    fontSize: 14,
-    color: '#ffffff',
-  },
-  fontEngInput: {
-    fontFamily: 'Kanit_400Regular',
-    borderRadius: '30px',
-    width: 280,
-    backgroundColor: 'transparent',
-  },
-  fontTh: {
-    fontFamily: 'Kanit_400Regular',
-  },
-  buttonStyle: {
-    backgroundColor: '#4542C1',
-    borderColor: 'transparent',
-    borderWidth: 0,
-    borderRadius: "9000px",
-    width: 240,
-    color: 'red'
-  },
-  // containsFilter: {
-  //   display: 'flex',
-  //   flexDirection: 'column'
-  // },
-  containerFilter: {
-    alignItems: 'center',
-    // height: '100%', width: '100%' ,
-    backgroundColor: 'white',
-    flexDirection: 'row',
-  },
-  vwSearch: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 40,
-    backgroundColor: '#4542C1',
-    borderRadius: '30px',
-  },
-  searchContainer:
-  {
-    height: 40,
-    width: '100%',
-    backgroundColor: '#4542C1',
-    // borderRadius: '30px', 
-  },
-  card: {
-    padding: 10,
-    backgroundColor: 'yellow',
-    borderRadius: "15px",
-    borderColor: "transparent",
-  },
-  containerCardparty: {
-    width: '90%',
-  },
-  row: {
-    flexWrap: "wrap",
-    display: "flex",
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    width: '100%',
-    height: '10'
-  },
-  column3: {
+    },
+    tabContainer: {
+      height: '100%',
+      width: '100%',
+      alignItems: 'center',
+      backgroundColor: 'transparent',
+      paddingTop: '20px'
+    },
+    tabBar: {
+      backgroundColor: 'white',
+    },
+    fontEng: {
+      fontFamily: 'Kanit_400Regular',
+      fontSize: 14,
+    },
+    fontEngInputHeader: {
+      fontFamily: 'Kanit_400Regular',
+      fontSize: 14,
+      color: '#ffffff',
+    },
+    fontEngInput: {
+      fontFamily: 'Kanit_400Regular',
+      borderRadius: '30px',
+      width: 280,
+      backgroundColor: 'transparent',
+    },
+    fontTh: {
+      fontFamily: 'Kanit_400Regular',
+    },
+    buttonStyle: {
+      backgroundColor: '#4542C1',
+      borderColor: 'transparent',
+      borderWidth: 0,
+      borderRadius: "9000px",
+      width: 240,
+      color: 'red'
+    },
+    // containsFilter: {
+    //   display: 'flex',
+    //   flexDirection: 'column'
+    // },
+    containerFilter: {
+      alignItems: 'center',
+      // height: '100%', width: '100%' ,
+      backgroundColor: 'white',
+      flexDirection: 'row',
+    },
+    vwSearch: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 40,
+      backgroundColor: '#4542C1',
+      borderRadius: '30px',
+    },
+    searchContainer:
+    {
+      height: 40,
+      width: '100%',
+      backgroundColor: '#4542C1',
+      // borderRadius: '30px', 
+    },
+    card: {
+      padding: 10,
+      backgroundColor: 'yellow',
+      borderRadius: "15px",
+      borderColor: "transparent",
+    },
+    containerCardparty: {
+      width: '90%',
+    },
+    row: {
+      flexWrap: "wrap",
+      display: "flex",
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      width: '100%',
+      height: '10'
+    },
+    column3: {
       width: "25%"
-  },
-  column9: {
+    },
+    column9: {
       width: "75%"
-  }
-});
+    }
+  });
 
-export default FindParty
+  export default FindParty
